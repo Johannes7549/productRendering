1c32cf1493adc124846e2535f57e57d41a6c5da2
Yohannes <yohannesbekele107@gmail.com>
2023-11-05 00:07:37 +0300

    Product specification changed

--- src/components/product.js
+++ src/components/product.js
@@ -9,7 +9,7 @@ function Products(props){
               <div class="media-body order-2 order-lg-1">
                 <h5 class="mt-0 font-weight-bold mb-2">{props.name}</h5>
                 <p class="font-italic text-muted mb-0 small">{props.description}</p>
-                <ProductDetails price = {props.price} isAvailable = {props.isAvailable} productLimit = {props.maxLimit}></ProductDetails>
+                <ProductDetails price = {props.price} isAvailable = {props.isAvailable}></ProductDetails>
               </div><img src={props.imgURL} alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
               </div>
           </li>
