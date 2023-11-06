6b5fa3f28c63c1a3ce6463c1a42900554beccb39
Yohannes <yohannesbekele107@gmail.com>
2023-11-06 21:59:55 +0300

    form updated

--- src/components/createproduct.js
+++ src/components/createproduct.js
@@ -9,11 +9,15 @@ function CreateProduct(props){
     function changeShowForm(){
         setShowForm(true)
     }
+    function cancelFrom(){
+        setShowForm(false)
+    }
    return (
 
         <div className="row">
             <div className="col-lg-8 mx-auto" style={{background:"white",paddingBottom:30,marginBottom:20}}>
-              { !showForm && <button className="btn btn-primary" onClick={changeShowForm}>Create</button>}
+                {!showForm && <button className="btn btn-primary" onClick={changeShowForm}>Create</button>}
+                {showForm && <button className="btn btn-primary" onClick={cancelFrom}>Cancel</button>}
                 {showForm && <ProductForm onCreateProduct = {createProduct}></ProductForm>}
             </div>
        </div>
