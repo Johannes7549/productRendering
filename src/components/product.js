6da7dfb07ca719a7d6c57b762071e816dd0740c7
Yohannes <yohannesbekele107@gmail.com>
2023-11-07 12:24:11 +0300

    Product changed

--- src/components/product.js
+++ src/components/product.js
@@ -6,9 +6,9 @@ function Products(props){
         <ul class="list-group shadow">
           <li class="list-group-item">
             <div class="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
-              <div class="media-body order-2 order-lg-1">
+              <div class="media-body order-2 order-lg-1" style={{color:"white"}}>
                 <h5 class="mt-0 font-weight-bold mb-2">{props.name}</h5>
-                <p class="font-italic text-muted mb-0 small">{props.description}</p>
+                <p class="font-italic mb-0 small" style={{color:"white"}}>{props.description}</p>
                 <ProductDetails price = {props.price} isAvailable = {props.isAvailable}></ProductDetails>
               </div><img src={props.imgURL} alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
               </div>
